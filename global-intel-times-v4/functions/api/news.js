0ff47e6ba28d06d34225b311ef6772d83c0da54f
karangadhiya07-ship-it <karangadhiya07@gmail.com>
2026-06-26 19:18:42 +0530

    Update news.js

--- global-intel-times-v4/functions/api/news.js
+++ global-intel-times-v4/functions/api/news.js
@@ -6,17 +6,12 @@ const RSS_FEEDS = [
   'https://news.google.com/rss/search?q=politics&hl=en-US&gl=US&ceid=US:en',
   'https://news.google.com/rss/search?q=business&hl=en-US&gl=US&ceid=US:en',
   'https://news.google.com/rss/search?q=technology&hl=en-US&gl=US&ceid=US:en',
-  'https://news.google.com/rss/search?q=artificial%20intelligence&hl=en-US&gl=US&ceid=US:en',
-  'https://news.google.com/rss/search?q=crypto&hl=en-US&gl=US&ceid=US:en',
   'https://news.google.com/rss/search?q=sports&hl=en-US&gl=US&ceid=US:en',
   'https://news.google.com/rss/search?q=health&hl=en-US&gl=US&ceid=US:en',
-  'https://news.google.com/rss/search?q=science&hl=en-US&gl=US&ceid=US:en',
 
   'https://feeds.bbci.co.uk/news/rss.xml',
   'https://feeds.bbci.co.uk/news/world/rss.xml',
   'https://feeds.bbci.co.uk/news/business/rss.xml',
-  'https://feeds.bbci.co.uk/news/technology/rss.xml',
-  'https://feeds.bbci.co.uk/sport/rss.xml',
 
   'https://www.theguardian.com/us/rss',
   'https://www.theguardian.com/world/rss',
@@ -26,38 +21,10 @@ const RSS_FEEDS = [
 
   'https://www.npr.org/rss/rss.php?id=1001',
   'https://www.npr.org/rss/rss.php?id=1003',
-  'https://www.npr.org/rss/rss.php?id=1006',
-  'https://www.npr.org/rss/rss.php?id=1007',
 
   'https://www.cnbc.com/id/100003114/device/rss/rss.html',
-  'https://www.cnbc.com/id/19854910/device/rss/rss.html',
   'https://feeds.skynews.com/feeds/rss/world.xml',
-  'https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml',
-  'https://rss.nytimes.com/services/xml/rss/nyt/World.xml',
-  'https://rss.nytimes.com/services/xml/rss/nyt/Business.xml',
-  'https://rss.nytimes.com/services/xml/rss/nyt/Technology.xml',
-  'https://rss.nytimes.com/services/xml/rss/nyt/Sports.xml',
-
-  'https://feeds.a.dj.com/rss/RSSWorldNews.xml',
-  'https://feeds.a.dj.com/rss/RSSMarketsMain.xml',
-  'https://feeds.a.dj.com/rss/WSJcomUSBusiness.xml',
-  'https://feeds.arstechnica.com/arstechnica/index',
-  'https://www.theverge.com/rss/index.xml',
-  'https://techcrunch.com/feed/',
-  'https://www.wired.com/feed/rss',
-  'https://www.espn.com/espn/rss/news',
-  'https://www.aljazeera.com/xml/rss/all.xml',
-  'https://www.vox.com/rss/index.xml',
-  'https://www.politico.com/rss/politics-news.xml',
-  'https://www.axios.com/feeds/feed.rss',
-  'https://time.com/feed/',
-  'https://www.usatoday.com/news/rss/',
-  'https://abcnews.go.com/abcnews/topstories',
-  'https://www.cbsnews.com/latest/rss/main',
-  'https://www.nbcnews.com/id/3032091/device/rss/rss.xml',
-  'https://www.forbes.com/business/feed/',
-  'https://fortune.com/feed/',
-  'https://mashable.com/feeds/rss/all'
+  'https://www.aljazeera.com/xml/rss/all.xml'
 ];
 
 export async function onRequestGet({ request, env }) {
