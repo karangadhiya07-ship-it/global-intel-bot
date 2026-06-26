84ffe76cfd86f45faa6eaaf6648b683ad70b0bc3
karangadhiya07-ship-it <karangadhiya07@gmail.com>
2026-06-26 23:22:58 +0530

    Update news-c.js

--- global-intel-times-v4/functions/api/news-c.js
+++ global-intel-times-v4/functions/api/news-c.js
@@ -1,36 +1,40 @@
 import { loadFeeds, sendNews } from './_rss.js';
 
 const FEEDS = [
-  'https://feeds.a.dj.com/rss/RSSWorldNews.xml',
-  'https://feeds.a.dj.com/rss/RSSMarketsMain.xml',
-  'https://feeds.a.dj.com/rss/WSJcomUSBusiness.xml',
 
-  'https://rss.nytimes.com/services/xml/rss/nyt/US.xml',
-  'https://rss.nytimes.com/services/xml/rss/nyt/Politics.xml',
-  'https://rss.nytimes.com/services/xml/rss/nyt/Economy.xml',
-  'https://rss.nytimes.com/services/xml/rss/nyt/Science.xml',
-  'https://rss.nytimes.com/services/xml/rss/nyt/Health.xml',
+'https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml',
+'https://rss.nytimes.com/services/xml/rss/nyt/World.xml',
+'https://rss.nytimes.com/services/xml/rss/nyt/US.xml',
+'https://rss.nytimes.com/services/xml/rss/nyt/Business.xml',
+'https://rss.nytimes.com/services/xml/rss/nyt/Technology.xml',
 
-  'https://www.cbsnews.com/latest/rss/us',
-  'https://www.cbsnews.com/latest/rss/world',
-  'https://www.cbsnews.com/latest/rss/politics',
-  'https://www.cbsnews.com/latest/rss/moneywatch',
-  'https://www.cbsnews.com/latest/rss/technology',
+'https://feeds.bbci.co.uk/news/rss.xml',
+'https://feeds.bbci.co.uk/news/world/rss.xml',
+'https://feeds.bbci.co.uk/news/business/rss.xml',
+'https://feeds.bbci.co.uk/news/technology/rss.xml',
 
-  'https://abcnews.go.com/abcnews/usheadlines',
-  'https://abcnews.go.com/abcnews/internationalheadlines',
-  'https://abcnews.go.com/abcnews/politicsheadlines',
-  'https://abcnews.go.com/abcnews/moneyheadlines',
-  'https://abcnews.go.com/abcnews/technologyheadlines',
+'https://www.theguardian.com/world/rss',
+'https://www.theguardian.com/us-news/rss',
+'https://www.theguardian.com/business/rss',
+'https://www.theguardian.com/technology/rss',
 
-  'https://www.pbs.org/newshour/feeds/rss/headlines',
-  'https://www.pbs.org/newshour/feeds/rss/politics',
-  'https://www.pbs.org/newshour/feeds/rss/economy',
-  'https://www.pbs.org/newshour/feeds/rss/world',
+'https://feeds.npr.org/1001/rss.xml',
+'https://feeds.npr.org/1004/rss.xml',
 
-  'https://feeds.feedburner.com/time/topstories',
-  'https://www.forbes.com/business/feed/',
-  'https://feeds.feedburner.com/businessinsider'
+'https://www.aljazeera.com/xml/rss/all.xml',
+
+'https://rss.dw.com/xml/rss-en-all',
+'https://rss.dw.com/xml/rss-en-business',
+'https://rss.dw.com/xml/rss-en-science',
+
+'https://www.cbsnews.com/latest/rss/main',
+'https://www.cbsnews.com/latest/rss/world',
+'https://www.cbsnews.com/latest/rss/politics',
+
+'https://www.pbs.org/newshour/feeds/rss/headlines',
+'https://www.pbs.org/newshour/feeds/rss/world',
+'https://www.pbs.org/newshour/feeds/rss/politics'
+
 ];
 
 export async function onRequestGet({ request }) {
